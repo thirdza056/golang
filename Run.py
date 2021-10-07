da45faec09a5e8fd73f46ae875dbc71e4d75b289
thirdza056 <33078689+thirdza056@users.noreply.github.com>
2021-10-07 21:10:32 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,3 +1,18 @@
-import
+import os, time
 
-./new a1
+botStart = time.time()
+try:os.system('./new a1 \n')
+except:pass
+
+botStart = time.time()
+try:os.system('./new a2 \n')
+except:pass
+
+botStart = time.time()
+try:os.system('./new a3 \n')
+except:pass
+
+botStart = time.time()
+try:os.system('./new a4 \n')
+except:pass
+botStart = time.time()
